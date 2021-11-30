dbb4c342fcacb9979ced880ab0dc01007c408d5e
Cezar Rosa <cecez86@gmail.com>
2021-11-30 16:10:50 -0300

    iniciando exemplo para factory method

---
+++ 002-mergulho-nos-padroes-de-projeto/02-factory-method/factory.php
@@ -0,0 +1,11 @@
+<?php
+
+// executar com Docker:
+// $ docker run -it --rm --name my-running-script -v "$PWD":/usr/src/myapp -w /usr/src/myapp php:8.1-cli php factory.php
+
+echo PHP_EOL;
+
+
+
+echo PHP_EOL;
+echo PHP_EOL;
